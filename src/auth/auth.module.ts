2a590aaeaa09919936246f7609828ceff0fc9b70
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2022-01-11 17:32:54 -0300

    At código

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      // SEGREDO DA APLICAÇÃO.
+      secret: 'r&n&q&*Ne&lfZ5^yEH2!%U6oUbDmUOchUr7tgdXVcGdg!9Xz9q',
+      // DEFINE A QUANTIDADE DE TEMPO QUE O TOKEN VALE.
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [PrismaService, AuthService],
   controllers: [AuthController],
 })
