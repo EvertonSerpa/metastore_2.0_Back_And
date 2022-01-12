91a5592316bad9cdddc7319dbed456967f2a8a6f
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2022-01-12 17:57:21 -0300

    at prisma

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -6,13 +6,9 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    super();
-  }
   async onModuleInit() {
     await this.$connect();
   }
-
   async onModuleDestroy() {
     await this.$disconnect();
   }
